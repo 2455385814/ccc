b2c6840f42bc15739679653fe7cec1826ea894d4
hl666 <2455385814@qq.com>
2021-11-26 14:35:44 +0800

    Create strcpy函数代码.cpp

---
+++ strcpy函数代码.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<string.h>
+char*strcopy(char*src){
+	int length=strlen(src);
+	char *p;
+	p=(char*)malloc((length+1)*sizeof(char));
+	for(int i=0;i<length;i++){
+		p[i]=src[i];
+	}
+	p[length]='\0';
+	return p;
+}
